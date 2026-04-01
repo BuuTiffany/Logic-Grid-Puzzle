a8c4c6cc682936c1654063e7393aca5118380caf
Michaeldo2004 <michaeldo2004@csu.fullerton.edu>
2026-03-31 22:14:49 -0700

    added some routes

--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -1,4 +1,4 @@
-<p>{"Home"}</p>
+<h1>Leaderboard</h1>
 
 <script>
   import { PUBLIC_API_BASE } from '$env/static/public';
@@ -7,7 +7,7 @@
   let message = '';
 
   onMount(async () => {
-    const res = await fetch(`${PUBLIC_API_BASE}/api/hello/`);
+    const res = await fetch(`${PUBLIC_API_BASE}/api/leaderboard/`);
     const data = await res.json();
     message = data.message;
   });
